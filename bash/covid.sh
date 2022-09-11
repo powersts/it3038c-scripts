ce9bbbec87b99a1fd3ac87e1a80264faa6a92049
powersts <powersts@mail.uc.edu>
2022-09-11 18:51:30 -0400

    Lab 2

---
+++ bash/covid.sh
@@ -0,0 +1,8 @@
+DATA=$(curl https://api.covidtracking.com/v1/us/current.json)
+POSITIVE=$(echo $DATA | jq '.[0].positive')
+NEGATIVE=$(echo $DATA | jq '.[0].negative')
+DEATHS=$(echo $DATA | jq '.[0].deaths')
+HOSPITALIZED=$(echo $DATA |jq '.[0].hospitalized')
+TODAY=$(date)
+
+echo "On $TODAY, there were $POSITIVE positive cases, $NEGATIVE negative tests,$DEATHS deaths, and $HOSPITALIZED hospitalized."
