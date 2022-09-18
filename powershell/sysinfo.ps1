3d8cfbe097a28381769d0dd4aec7e59b1c9a7832
powersts <powersts@mail.uc.edu>
2022-09-18 18:17:18 -0400

    Sysinfo Script

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,8 +1,12 @@
 ﻿function getIP{
     (Get-NetIPAddress).IPv4Address | Select-String "192*"
     }
-    Write-Host(getIP)
 $IP = getIP
-Write-Host("This machine's IP is $IP")
-
-Write-Host("This machine's IP is {0}" -f $IP)
+$USER = $env:USERNAME
+$HOSTNAME = $env:COMPUTERNAME
+$VERSION = $HOST.Version.Major
+$DATE = Get-Date -Format "dddd MM/dd/yyyy"
+$BODY = "This machine's IP is $IP. User is $USER. Hostname is $HOSTNAME. Powershell Version $VERSION. Today's date it $DATE."
+Write-Host("This machine's IP is $IP. User is $USER. Hostname is $HOSTNAME. Powershell Version $VERSION. Today's date it $DATE.")
+#Send-MailMessage -To "reedws@ucmail.uc.edu" -From "troypowers111@gmail.com" -Subject "IT Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
+"$BODY" | Out-File -FilePath .\powershell\sysinfo.txt
